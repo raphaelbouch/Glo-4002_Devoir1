a6824a695b96bda98e84795632cc50c15a6b0a7b
Raphael <raphael.bouchard.5@ulaval.ca>
2019-09-09 14:13:59 -0400

    Fin etape 1

--- src/main/java/Clinic.java
+++ src/main/java/Clinic.java
@@ -30,4 +30,36 @@ public class Clinic {
     	
     	return RequireRadiology;
     }
+    
+    public boolean PatientIsInMedecinQueue(String name) {
+    	if (FileMedecin.contains(name)) {
+    		return true;
+    	} else {
+    		return false;
+    	}
+    }
+    
+    public boolean PatientIsInRadiologyQueue(String name) {
+    	if (FileRadiologie.contains(name)) {
+    		return true;
+    	} else {
+    		return false;
+    	}
+    }
+    
+    public String GetPatientMededcinByNumberInQueue(int number) {
+    	if (FileMedecin.size() <= number) {
+    		return null;
+    	} else {
+    		return FileMedecin.get(number);
+    	}
+    }
+    
+    public String GetPatientRadiologyByNumberInQueue(int number) {
+    	if (FileRadiologie.size() <= number) {
+    		return null;
+    	} else {
+    		return FileRadiologie.get(number);	
+    	}
+    }
 }
--- src/test/java/ClinicTest.java
+++ src/test/java/ClinicTest.java
@@ -5,16 +5,40 @@ import org.junit.Test;
 
 public class ClinicTest {
 	
+	private static final String FIRST_PATIENT_NAME = "John";
+	private static final int UNSPECIFIED_GRAVITY = 0;
+	private static final String SECOND_PATIENT_NAME = "Luke";
 	public Clinic clinic;
 	
 	@Before
-	public void testClinic() {
+	public void setup() {
 		clinic = new Clinic();
 	}
 
 	@Test
-	public void testTriagePatient() {
-		fail("Not yet implemented");
+	public void WhenPatientComeWithMigraine_ThenHeIsFirstInMedecinQueue_AndNotInRadiologyQueue() {
+		clinic.triagePatient(FIRST_PATIENT_NAME, UNSPECIFIED_GRAVITY, VisibleSymptom.MIGRAINE);
+		
+		assertTrue(clinic.GetPatientMededcinByNumberInQueue(0).equals(FIRST_PATIENT_NAME));
+		assertFalse(clinic.PatientIsInRadiologyQueue(FIRST_PATIENT_NAME));
+		
+	}
+	
+	@Test
+	public void IfThereIsAPatientInMedecinQueue_WhenPatientComeWithFlu_ThenHeIsSecondInMedecinQueue_AndNotInRadiologyQueue() {
+		clinic.triagePatient(FIRST_PATIENT_NAME, UNSPECIFIED_GRAVITY, VisibleSymptom.MIGRAINE);
+		clinic.triagePatient(SECOND_PATIENT_NAME, UNSPECIFIED_GRAVITY, VisibleSymptom.FLU);
+		
+		assertTrue(clinic.GetPatientMededcinByNumberInQueue(1).equals(SECOND_PATIENT_NAME));
+		assertFalse(clinic.PatientIsInRadiologyQueue(SECOND_PATIENT_NAME));
+	}
+	
+	@Test
+	public void WhenPatientComeWithSprain_ThenHeIsFirstInMedecinQueue_AndHeIsFirstInRadiologyQueue() {
+		clinic.triagePatient(FIRST_PATIENT_NAME, UNSPECIFIED_GRAVITY, VisibleSymptom.SPRAIN);
+		
+		assertTrue(clinic.GetPatientMededcinByNumberInQueue(0).equals(FIRST_PATIENT_NAME));
+		assertTrue(clinic.GetPatientRadiologyByNumberInQueue(0).equals(FIRST_PATIENT_NAME));
 	}
 
 }
